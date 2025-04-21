0f3eacf7b30077014cf79a723afd751c29608a07
Sweta Sharma <swetasharma@hotmail.com>
2025-04-21 01:39:36 -0500

    Adding code for Parameterized build via Maven/cmd

--- Framework/src/test/java/com/learnautomation/pages/BaseClass.java
+++ Framework/src/test/java/com/learnautomation/pages/BaseClass.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -45,12 +46,14 @@ public ExtentTest logger;
      Reporter.log("Setting done- Test can be started..", true);
     }
 	
+    @Parameters({"browser", "urlToBeTested"})
 	@BeforeClass
-	public void setUp() {
+	public void setUp(String browser, String url) {
 		
 		Reporter.log("Trying to start browser and getting application ready..", true);
 		 
-		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingURL());
+	// driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingURL());
+		driver = BrowserFactory.startApplication(driver, browser, url);
 		System.out.println(driver.getTitle());
 		
 		 Reporter.log("Browser and application is up and running..", true);
